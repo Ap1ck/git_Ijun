97f757f005a5166be3678a7ced554be651db5c74
Alexandr P <He1ton@yandex.ru>
2021-06-29 21:48:50 +0300

    Task 3 block one

---
+++ HomeWork_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HomeWork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturAll = 52;
+            int picturSeries = 3;
+            int series = picturAll / picturSeries;
+            int aboveNorm = picturAll % series;
+            Console.WriteLine($"Количество заполненых рядов: {series} , картинок сверх нормы {aboveNorm}");
+        }
+    }
+}
